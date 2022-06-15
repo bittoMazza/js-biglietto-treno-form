c0d42679a42465f5478423856754ad7e1a8d7cae
bittoMazza <robertomazza99@gmail.com>
2022-06-15 15:30:38 +0200

    add "select" tag on age input

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 let userNumKm;
 let userAge;
 let ticketPrice;
+let userCompleteName
 
 // Nel momento in cui si clicca il bottone,vengono presi gli input dell'utente
 const submitBtn = document.getElementById('submit-btn');
 submitBtn.addEventListener('click',function(){
+    userCompleteName = document.getElementById('user-complete-name').value;
     userNumKm = parseInt(document.getElementById('user-km').value);
-    userAge = parseInt(document.getElementById('user-age').value);
+    userAge = document.getElementById('user-age').value;
     ticketPrice = userNumKm * 0.26;
     // Ipotizziamo che non esistano stazioni distanti meno di 5 KM l'una dall'altra
     if (isNaN(userNumKm) == true || userNumKm < 5) 
@@ -16,18 +18,12 @@ submitBtn.addEventListener('click',function(){
     return;
     } 
     //Ipotizziamo che non esistano persone con piu di 120 anni
-    else if(isNaN(userAge) == true || (userAge < 0 || userAge > 120))
-    {
-        alert('ATTENZIONE : ETA\' NON VALIDA, RICARICARE LA PAGINA');
-        console.warn('ATTENZIONE : ETA\' NON VALIDA, RICARICARE LA PAGINA');
-        return;
-    } 
     else{
-        if(userAge < 18)
+        if(userAge == 'Minorenne')
         {
             ticketPrice=ticketPrice-((ticketPrice * 15)/100);    
         }
-        else if(userAge >= 65)
+        else if(userAge == 'over')
         {
             ticketPrice=ticketPrice-((ticketPrice * 35)/100);
         }
@@ -35,7 +31,7 @@ submitBtn.addEventListener('click',function(){
 
     ticketPrice = ticketPrice.toFixed(2);
     console.clear();
-    console.log('Il prezzo del biglietto è : €'+ ticketPrice)
+    console.log('Signor/a '+userCompleteName+' il prezzo del biglietto è : €'+ ticketPrice)
 
 })
 
